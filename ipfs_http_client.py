cdbba75951bf3103faa2563ff037f960c4087dfb
Twodragon0 <Twodragon0@example.com>
2020-03-12 08:42:53 +0000

    add branch and python3 socket

---
+++ ipfs_http_client.py
@@ -0,0 +1,18 @@
+import ipfshttpclient
+
+# Share TCP connections using a context manager
+with ipfshttpclient.connect() as client:
+	hash = client.add('data/test.txt')['Hash']
+	print(hash)
+
+# Share TCP connections until the client session is closed
+class SomeObject:
+	def __init__(self):
+		self._client = ipfshttpclient.connect(session=True)
+
+	def do_something(self):
+		hash = self._client.add('data/test.txt')['Hash']
+		print(self._client.stat(hash))
+
+	def close(self):  # Call this when your done
+		self._client.close()
